7ae4525111a22455b4e4eefc76eab9c05b8b8f89
Stuart <stuartlee165@outlook.com>
2022-03-15 21:39:38 +0000

    app added

---
+++ app/app.py
@@ -0,0 +1,69 @@
+# Importing Necessary packages
+from flask import Flask,render_template, request, jsonify
+import numpy as np
+from fastai.vision import *
+from fastbook import *
+from fastai.vision.widgets import *
+
+import pickle
+import io
+import os
+
+
+# path = Path('imagepath4')
+# path = 1
+# fns = get_image_files(path)
+
+
+# Initialiazing flask app
+app = Flask(__name__)
+
+cwd = os.getcwd()
+path= cwd + '/model'
+
+print(path)
+
+address = path + '/model.pkl'
+print(address)
+# path = Path('model')
+
+
+#Loading  saved model
+# model = load_learner(path/'model.pkl')
+
+model = load_learner(address)
+
+
+
+
+
+
+# Rendering index.html at /
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Getting data with POST Method
+@app.route('/upload', methods=["POST"])
+def upload():
+    # try:
+        # Getting img from POST
+        file = request.files['user-img'].read()
+        # Resizing img to 224 X 224 , This is the size on which model was trained
+#         img = open_image(io.BytesIO(file))
+        img = PILImage.create(file)
+        # Prediction using model
+        prediction = model.predict(img)[0]
+
+        # Getting Prediction ready to sent it to frontend
+        response = {"result": str(prediction)}
+        return jsonify(response)
+
+### Routing option 1
+@app.route('/hello')
+def hello_world():
+   return 'hello world'
+
+#running app at localhost on port 8080
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
---
+++ app/tests.py
@@ -0,0 +1,6 @@
+# setting working dir
+
+import os
+cwd = os.getcwd()
+path= cwd + '/model'
+print(path)
